26e5a14c5f22d3a8a6e6667829d64d6a2213aa88
laviniarossito <laviniarossito.etec@gmail.com>
2025-11-28 10:42:10 -0300

    Alteração da ClasseOculosSol

--- src/ClasseOculosSol.java
+++ src/ClasseOculosSol.java
@@ -1,4 +1,23 @@
 
-public class ClasseOculosSol {
+public class ClasseOculosSol extends ClasseProdutos {
 
+	String corLente;
+	
+	// Construtor 
+	public ClasseOculosSol(String cor, float preco, String pigmentacao){
+		super(cor, preco);
+		
+		this.corLente = pigmentacao;
+		
+	}
+	
+	public void setCorLente(String pigmento){
+		this.corLente = pigmento;
+
+	}
+	
+	public String getCorLente(){
+		return corLente;
+	}
+	
 }
--- src/ClasseProdutos.java
+++ src/ClasseProdutos.java
@@ -1,4 +1,30 @@
 
 public class ClasseProdutos {
 
+	String cor;
+	float preco;
+	
+	// Construtor
+	public ClasseProdutos(String corPar, float precoPar){
+		
+		this.cor = corPar;
+		this.preco = precoPar;
+	}
+	
+	public String getCor(){
+		return cor;
+	}
+
+	public float getPreco(){
+		return preco;
+	}
+	
+	public void setCor(String color){
+		this.cor = color;
+	}
+
+	public void setPreco(float price){
+		this.preco = price;
+	}
+
 }
